4b50696da49f2bf135042c423274e5f9794b8d18
Mikhail <mihailsavuskin13@gmail.com>
2023-01-17 18:59:41 +0300

    015-15

--- 015.String/14/app.js
+++ 015.String/14/app.js
@@ -1,7 +1,7 @@
 // На вход подается url. Если он начинается с http, содержит хотя бы один “/” и
 // заканчивается подстрокой .com или .ru, то вывести true, в противном случае false
 const url = prompt('URL').trim();
-if (url.slice(0,4) ==='http' && url.lastIndexOf()) {
+if (url.slice(0,4) ==='http' && url.lastIndexOf('.ru')>0|| url.lastIndexOf('.com')>0 && url.indexOf('/')>0 ) {
     console.log(true);
 }
 else {
