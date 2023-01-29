fdf1f9f99c1fd4e96821b1448250deae63d63bd8
Mikhail <mihailsavuskin13@gmail.com>
2023-01-29 22:31:10 +0300

    19

---
+++ 019.Cycles/7/app.js
@@ -0,0 +1,26 @@
+// Дан массив с элементами [1, 2, 3, 4, 5]. С помощью цикла for of найдите сумму
+// элементов этого массива
+
+const arr = [1, 2, 3, 4, 5];
+let res = 0;
+
+// for (let i = 0; i < arr.length; i++) {
+//     res += arr[i]
+// } console.log(res);
+
+// let i = 0;
+// while (i < arr.length) {
+//     res += arr[i]
+//     i++
+// } console.log(res);
+
+// let i = 0;
+// do {
+//     res += arr[i];
+//     i++
+// } while (i < arr.length)
+// console.log(res);
+
+for (let i of arr){
+    res+=i;
+}console.log(res);
