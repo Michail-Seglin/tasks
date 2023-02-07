ae111d2cc554a8b43fefe4075b553cb860f0a5c1
Michail-Seglin <mihailsavuskin13@gmail.com>
2023-02-07 16:27:02 +0300

    20-16

--- 020.Function/16/app.js
+++ 020.Function/16/app.js
@@ -9,25 +9,19 @@
 // 4 -> [[1, 2, 3, 4], [5, 6]
 // 5 -> [[1, 2, 3, 4, 5], [6]]
 // 6 -> [[1, 2, 3, 4, 5, 6]]
-const arr = [1, 2, 3, 4, 5, 6];
-const arrDub = [1, 2, 3, 4, 5, 6];
-let n = 0;
-let arr21 = [arr[0], arr[1]];
-let arr22 = [arr[2], arr[3]];
-let arr23 = [arr[4], arr[5]];
-let arr2 = [];
-arr2.push(arr21)
-arr2.push(arr22)
-arr2.push(arr23)
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const res = [];
+let n = +prompt('input number');
+let item = [];
 for (let i = 0; i < arr.length; i++) {
-    if (i < 2) {
-        arr21.push(arr[i])
+    item.push(arr[i]);
+    if (item.length === n) {
+        res.push(item);
+        item = [];
     }
-    else if (i >= 2 && i < 4) {
-        arr22.push(arr[i]);
+    if ((i === arr.length - 1) && (arr.length % n !== 0)) {
+        res.push(item);
     }
-    else if (i >= 4) { arr23.push(arr[i]); }
 }
-
-console.log(arr2);
+console.log(res);
 
