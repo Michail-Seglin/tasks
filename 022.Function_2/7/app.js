9517325ca1310599fc23c326a151468239c5f15d
Mikhail <mihailsavuskin13@gmail.com>
2023-02-08 22:41:27 +0300

    22-7

--- 022.Function_2/7/app.js
+++ 022.Function_2/7/app.js
@@ -1,14 +1,25 @@
-// . На вход статичный объект. Необходимо объект проверить на пустоту. Если же
-// объект пустой, то выводим false, если же содержит хотя бы одну пару (ключ:
-// значение), то true
-const obj = {};
-let count = 0;
-
-for (let key in obj) {
-    if (key) {
-        count++
+// На входе массив. Реализуйте 2 функции. Первая для проверки, что в массиве
+// только числа. Вторая для получения суммы всех элементов массива. Если
+// результат функции проверки – true, то вызывать новую функцию, возвращающую
+// сумму всех элементов массива
+const arr = [1, 2, 4, 6];
+function showResult(arr_) {
+    let count = 0;
+    for (let i = 0; i < arr_.length; i++) {
+        if (!isNaN(arr_[i])) {
+            continue;
+        } else {
+            count += 1;
+        }
     }
+    if (count > 0) {
+        return false;
+    } else return true;
 }
-if (count === 0) {
-    console.log(false);
-} else { console.log(true); }
+
+const res = arr.reduce(function (sum, el) {
+    return  sum + el
+}, 0)
+
+
+console.log(res);
