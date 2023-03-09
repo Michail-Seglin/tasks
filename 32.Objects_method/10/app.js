f9973b67b789e34d53eb1a0950356e1489dfbbf3
Michail-Seglin <mihailsavuskin13@gmail.com>
2023-03-09 19:57:05 +0300

    1-7

--- 32.Objects_method/10/app.js
+++
@@ -1,7 +0,0 @@
-// const btn2 = document.querySelector('bt1');
-const btn1 = document.querySelector('button .bt2');
-
-btn1.addEventListener('click', function () {
-    let inp = document.querySelector('input');
-    inp.disabled = true;
-})
--- 32.Objects_method/7/app.js
+++ 32.Objects_method/7/app.js
@@ -1,17 +1,19 @@
-// Записать в результат удвоенное значение инпута. Добавить проверки 
+// На вход подается число. Необходимо каждое значение возвести в степень
+// индекса и вычислить сумму.
+// 12345 = 1
+// 0+2
+// 1+3
+// 2+4
+// 3+5
+// 4
 
-const btn = document.querySelector('button');
-
-btn.addEventListener('click', function () {
-
-    try {
-        const inp = document.querySelector('input');
-        const div = document.querySelector('div');
-
-        if (inp.value.trim() === '') throw new Error('Epmty')
-        if (isNaN(inp.value)) throw new Error('Input number')
-        div.innerHTML = inp.value * 2;
-    } catch (error) {
-        alert(error.message);
+const str = '1234567';
+function main(str_) {
+    let count = 0;
+    for (let i = 0; i < str_.length; i++) {
+        count += str_[i] ** i;
     }
-})
+    return count;
+}
+const res = main(str);
+console.log(res);
