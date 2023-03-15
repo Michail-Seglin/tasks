4fba771443aa75a56931d80cc291da4b7500f214
Michail-Seglin <mihailsavuskin13@gmail.com>
2023-03-15 16:33:13 +0300

    1-8

--- 34.OOP/8/app.js
+++ 34.OOP/8/app.js
@@ -1,7 +1,20 @@
-// У вас есть кнопка. После нажатия поменять background.
+// Реализуйте класс Validator. У него будет метод isEmail параметром принимает
+// строку и проверяет, является ли она корректным емейлом или нет. Если является
+// - возвращает true, если не является - то false.
 
-const btn = document.querySelector('button');
+class Validator {
+    str;
 
-btn.addEventListener('click', function () {
-    btn.style = 'background-color:red'
-})
+    isMail(str) {
+        try {
+            if (!/^[\w\.\-]+@[a-z]{1,10}\.(by|com)$/gm.test(str)) throw new Error('Невалидный EMAIL');
+            return true
+        } catch (er) {
+            return er.message
+        }
+    }
+}
+const validator = new Validator();
+const email = validator.isMail('misha_hp@tut.by');
+console.log(email);
+
