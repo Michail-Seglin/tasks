10fd8c8e55d4b302fccff46dfca32e1d55a98f02
Michail-Seglin <mihailsavuskin13@gmail.com>
2023-03-16 15:08:47 +0300

    1-11

--- 34.OOP/11/app.js
+++ 34.OOP/11/app.js
@@ -1,19 +1,61 @@
-// Напишите функцию, принимающую в качестве параметра статичный объект.
-// Функция возвращает новый объект, где значения – исключительно числа
-// первоначального объекта. IIFE
-const obj = {
-    1: 'misha',
-    2: 232,
-    3: 'seg',
-    4: 'bmw',
-    5: 2323
-};
-(function (obj_) {
-    let newObj = {};
-    for (let key in obj_) {
-        if (!isNaN(obj_[key])) {
-            newObj[key] = obj[key]
+// Реализуйте класс Validator. У него будет метод isEmail параметром принимает
+// строку и проверяет, является ли она корректным емейлом или нет. Если является
+// - возвращает true, если не является - то false.
+// Дополнить класс Validator. Добавить метод isURL для проверки на url.
+// Дополнить класс Validator. Добавить метод isDate для проверки на дату.
+//  Дополнить класс Validator. Добавить метод isPhone для проверки на номер
+// телефона.
+class Validator {
+    str;
+    url;
+    date;
+    phone;
+
+    isMail(str) {
+        try {
+            if (!/^[\w\.\-]+@[a-z]{1,10}\.(by|com)$/gm.test(str)) throw new Error('Невалидный EMAIL');
+            return true
+        } catch (er) {
+            return er.message
         }
     }
-    console.log(newObj);
-})(obj);
+
+    isURL(url) {
+        try {
+            if (!/^[a-z]+:\/\/[a-zA-Z0-9]+\.[a-z]+\.[a-z]{1,3}$/gm.test(url)) throw new Error('Некорректный URL');
+            return true
+        } catch (er) {
+            return er.message
+        }
+    }
+
+    isDate(date) {
+        try {
+            if (!/^[0-3]?[0-9].[0-3]?[0-9].(?:[0-9]{2})?[0-9]{2}$/gm.test(url)) throw new Error('Некорректная Дата');
+            return true
+        } catch (er) {
+            return er.message
+        }
+    }
+
+    isPhone(phone) {
+        try {
+            if (!/^[0-3]?[0-9].[0-3]?[0-9].(?:[0-9]{2})?[0-9]{2}$/gm.test(url)) throw new Error('Некорректная Дата');
+            return true
+        } catch (er) {
+            return er.message
+        }
+    }
+}
+const validator = new Validator();
+
+const email = validator.isMail('misha_hp@tut.by');
+console.log(email);
+
+const url = validator.isURL('https://www.onliner.by')
+console.log(url);
+
+const date = validator.isDate('20.1.2023');
+console.log(date);
+const phone = validator.isPhone('+375297654842')
+console.log(phone); 
--- 34.OOP/9/app.js
+++ 34.OOP/9/app.js
@@ -1,14 +1,32 @@
-// У вас есть кнопка. После каждого нажатия менять background.
+// Реализуйте класс Validator. У него будет метод isEmail параметром принимает
+// строку и проверяет, является ли она корректным емейлом или нет. Если является
+// - возвращает true, если не является - то false.
+// Дополнить класс Validator. Добавить метод isURL для проверки на url.
 
-const btn = document.querySelector('button');
-let flag = false;
-btn.addEventListener('click', function () {
+class Validator {
+    str;
+    url;
 
-    if (flag === false) {
-        btn.style = 'background-color:red';
-        flag = true;
-    } else {
-        btn.style = 'background-color:white';
-        flag = false
+    isMail(str) {
+        try {
+            if (!/^[\w\.\-]+@[a-z]{1,10}\.(by|com)$/gm.test(str)) throw new Error('Невалидный EMAIL');
+            return true
+        } catch (er) {
+            return er.message
+        }
     }
-})
+    isURL(url) {
+        try {
+            if (!/^[a-z]+:\/\/[a-zA-Z0-9]+\.[a-z]+\.[a-z]{1,3}$/gm.test(url)) throw new Error('Некорректный URL');
+            return true
+        } catch (er) {
+            return er.message
+        }
+    }
+}
+const validator = new Validator();
+const email = validator.isMail('misha_hp@tut.by');
+console.log(email);
+
+const url = validator.isURL('https://www.onliner.by')
+console.log(url);
